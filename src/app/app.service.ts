37dde91c54575943f3d60b3311927dd90827560d
5l0m1n5ky <130700114+5l0m1n5ky@users.noreply.github.com>
2025-02-28 12:57:15 +0100

    implemented globally httpClient handler to app.config.ts and created app.service.ts file to handle http requests to OMDB API

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,32 @@
 import { Injectable } from "@angular/core";
+import { environmet } from "../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { forkJoin, Observable, of, switchMap } from "rxjs";
+import { OmdbDetailsResponse } from "../models/omdb-details-response.model";
+import { Movie } from "../models/movie.model";
+import { OmdbResponse } from "../models/omdb-response.model";
 
 @Injectable({ providedIn: 'root' })
 
-export class AppService { 
-    
+export class AppService {
+
+    private apiUrl = environmet.apiUrl;
+    private apiKey = environmet.apiKey;
+
+    constructor(private httpClient: HttpClient) { }
+
+    searchResults(condition: string): Observable<OmdbDetailsResponse[]> {
+        return this.httpClient.get<OmdbResponse>(`${this.apiUrl}?apikey=${this.apiKey}&s=${condition}`).pipe(
+            switchMap(response => {
+
+                if (!response.Search) return of([]);
+
+                let detailedRequests: Observable<OmdbDetailsResponse>[] = response.Search.map((movieDetails: Movie) =>
+                    this.httpClient.get<OmdbDetailsResponse>(`${this.apiUrl}?apikey=${this.apiKey}&i=${movieDetails.imdbID}`)
+                );
+
+                return forkJoin(detailedRequests);
+            })
+        );
+    }
 }
